a31fb61c30788733c65b4768f2f560b06bc723cc
Maksim <maks07031997@yandex.ru>
2020-01-27 15:42:55 +0300

    Updates

---
+++ precache-manifest.291355cfbea1b618545176b232c0dc6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d80cd11efed131dce8f7cd86b8a3ac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec8c40bb809c853b2863",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "84a32fde82159e15494f",
+    "url": "/static/js/2.179b529b.chunk.js"
+  },
+  {
+    "revision": "f9a91717f1e7ad77e346666044cf178f",
+    "url": "/static/js/2.179b529b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec8c40bb809c853b2863",
+    "url": "/static/js/main.ab29f8d4.chunk.js"
+  },
+  {
+    "revision": "bddac1d17fe44740dbd5",
+    "url": "/static/js/runtime-main.7eb9a290.js"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/static/media/user.7a21cbe9.svg"
+  }
+]);
